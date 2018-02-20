10b95fe44d812ac69468b6861795fc17c394177b
Alessandro Miguel Sanchez <sanchez7@sh-ln03.stanford.edu>
2018-02-20 02:21:05 -0800

    eye gaze results

--- run.sh
+++ run.sh
@@ -4,9 +4,10 @@ module load system
 module load singularity/2.4
 
 vid=$SCRATCH/auto_gaze/raw_video/30054.f.25.V2-.mov
-#vid=$PI_HOME/headcam_videos_rotated/XS_1230_objs.mov
+out_dir=$PI_HOME/eyegaze
+out_file=results.features
 
 sbatch --mail-type=FAIL --mail-user=sanchez7@stanford.edu \
 	--wrap="singularity exec $SINGULARITY_CACHEDIR/openface-cambridge.img bash -c \
-	'cd /opt/OpenFace/build/bin && ./FeatureExtraction -q -out_dir $PI_HOME/eyegaze -f $SCRATCH/auto_gaze/raw_video/30054.f.25.V2-.mov'"
+	'cd /opt/OpenFace/build/bin && ./FeatureExtraction -q -f $vid -outroot $out_dir -of $out_file'"
 
