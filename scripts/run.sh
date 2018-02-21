825b3bfcde016d1abdeb62e92d686b3b5667ddd2
Alessandro Miguel Sanchez <sanchez7@sh-ln01.stanford.edu>
2018-02-20 20:18:06 -0800

    emily csv

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,9 +3,9 @@
 module load system
 module load singularity/2.4
 
-vid=$SCRATCH/auto_gaze/raw_video/socref_21018_03\ CV.MPG
+vid=$SCRATCH/auto_gaze/raw_video/socref_21018_03_CV.MPG
 out_dir=$PI_HOME/eyegaze
-out_file=results2.features
+out_file=results2.csv
 
 sbatch --mail-type=FAIL --mail-user=sanchez7@stanford.edu \
 	--wrap="singularity exec $SINGULARITY_CACHEDIR/openface-cambridge.img bash -c \
